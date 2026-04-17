d7b46a6db3b376a3ca07bf599df9f40068abdeac
Nick-McComb <128945764+Nick-McComb@users.noreply.github.com>
2026-04-17 15:53:09 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(const std::vector<int> & nums){
+    if(nums.size() < 2){
+        return false;
+    }
+
+    std::unordered_map<int, int> seen{};
+
+    for(const auto & n : nums){
+        if(seen.contains(n)){
+            return true;
+        }
+        seen[n] = 1;
+    }
+    return false;
+}
+};

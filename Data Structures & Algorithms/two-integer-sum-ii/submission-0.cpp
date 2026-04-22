31e8d9801dcc284cf052bbc36d5086c3342fc7ee
Nick-McComb <128945764+Nick-McComb@users.noreply.github.com>
2026-04-21 20:58:15 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        /*
+        get two indexs starting at the front and starting at the back
+        make loop that lowkey could loop infinitely 
+        check if the the numbers at the two indicies add up to the values
+
+         | |  | |
+        [1,2,3,4]
+        */
+
+        int start = 0;
+        int end = numbers.size()-1;
+
+        for(;;){
+            int total = numbers[start] + numbers[end];
+            if(total == target){
+                return std::vector<int> {start+1,end+1};
+            }
+            if(total < target){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+    }
+};

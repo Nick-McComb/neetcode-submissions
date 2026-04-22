3d6a366b41401933d629afa7ef9e66cfad6e0327
Nick-McComb <128945764+Nick-McComb@users.noreply.github.com>
2026-04-22 00:08:51 -0400

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        /*
+            make two pointers, one to the beginning of list and one to the end
+            get the min of the two pointers and multiple that by the difference between the index
+            set a new max area if larger than last
+            update the point that is small ( i think )
+        */
+
+        int start = 0;
+        int end= heights.size() -1;
+        int max_area = 0;
+        while(start < end){
+            int height = min(heights[start], heights[end]);
+            int width = end - start;
+            int area = height * width;
+            max_area = max(area, max_area);
+
+            if(heights[start] > heights[end]){
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+        return max_area;
+    }
+};

701e87cb77263bf5161637912ef58554bb515b88
Nick-McComb <128945764+Nick-McComb@users.noreply.github.com>
2026-04-25 14:55:04 -0400

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        stack<double> items{};
+        vector<pair<int, int>> ps{}; //{pos,speed}
+        
+        for(int i = 0; i< position.size(); i++){
+            ps.push_back(pair<int,int>{position[i], speed[i]});
+        }
+        std::sort(ps.begin(),ps.end(), [](pair<int,int> a, pair<int,int> b){ return a.first > b.first;});
+
+        for(int i = 0; i < ps.size(); i++){
+            double elm = double(target-ps[i].first)/double(ps[i].second);
+            cout<<elm<<endl;
+            if(items.empty()){
+                items.push(elm);
+                continue;
+            }
+            double prev = items.top();
+            items.push(elm);
+            if(elm <= prev) {
+                items.pop();
+            }
+        }
+        return items.size();
+    }
+};
